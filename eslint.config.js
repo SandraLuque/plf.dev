bf710dcfa2de4eed1f6ce79d634a91ac7cfaaf67
Sandra Veronica <sandrasarang77@gmail.com>
2025-02-12 13:55:03 -0500

    first commit setup

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import eslintPluginAstro from 'eslint-plugin-astro'
+
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    ignores: ['.astro/**/*.*'],
+    files: ['**/*.astro', '**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    rules: {
+      '@stylistic/jsx-quotes': ['error', 'prefer-double'],
+      '@stylistic/jsx-indent': 'off',
+      '@stylistic/space-before-function-paren': 'off',
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    },
+  },
+  {
+    files: ['**/*.astro'],
+    rules: {
+      'react/self-closing-comp': 'off',
+      'react/jsx-key': 'off',
+      '@stylistic/jsx-closing-tag-location': 'off',
+      '@stylistic/jsx-closing-bracket-location': 'off',
+      '@stylistic/multiline-ternary': 'off',
+    },
+  },
+]
